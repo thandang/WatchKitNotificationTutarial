5c61d6438a9c776156ac1e7c204d98876d836b8d
Than Dang <dtthan@apide.com>
2015-05-21 06:53:09 +0200

    Initial Commit

---
+++ WatkitNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatkitNotification
+//
+//  Created by Dang Thanh Than on 5/21/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatkitNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatkitNotification
+//
+//  Created by Dang Thanh Than on 5/21/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
