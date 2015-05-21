65392f94bcee2742c832bb0cd11174d6074390a3
Than Dang <dtthan@apide.com>
2015-05-21 09:10:34 +0200

    initial project

---
+++ WatkitNotification WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatkitNotification WatchKit Extension
+//
+//  Created by Dang Thanh Than on 5/21/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
